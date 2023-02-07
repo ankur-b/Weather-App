9523958ee719dd7dbc0e9b1fc57d41fc68c3463d
ankur-b <ankurbarve29@gmail.com>
2023-02-07 22:59:44 +0530

    customize http request

--- app.js
+++ app.js
@@ -1,9 +1,8 @@
 const request = require("request")
 const API_KEY = require("./Api_Key")
 
-const url = `http://api.weatherstack.com/forecast?access_key=${API_KEY}&query=22.689155253334622, 75.86502147469253`
+const url = `http://api.weatherstack.com/forecast?access_key=${API_KEY}&query=22.689155253334622, 75.86502147469253&units=f`
 
-request({url:url},(err,response)=>{
-    const data = JSON.parse(response.body)
-    console.log(data.current)
+request({url:url,json:true},(err,response)=>{
+    console.log(response.body.current.weather_descriptions[0] +'. It is currently ' + response.body.current.temperature+' degrees out. It feels like ' + response.body.current.feelslike + ' degress out.')
 })
